0b8c018666023ae00b8992bfdd111fbd12ee187c
Camilo Figueroa <cigotete@gmail.com>
2023-04-04 19:53:23 -0500

    Implemented code in Contact Controller and Vue component to create and store Contacts.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -24,6 +24,7 @@ class ContactController extends Controller
         $filters = $request->all('search');
         $contacts = Contact::with('organization')
         ->filter($filters)
+        ->latest('id')
         ->paginate(3);
 
         return Inertia::render('Contacts/Index', compact('contacts', 'filters'));
@@ -34,15 +35,33 @@ class ContactController extends Controller
      */
     public function create()
     {
-        return Inertia::render('Contacts/Create');
+        $organizations = \App\Models\Organization::all();
+        $countries = \App\Models\Country::all();
+
+        return Inertia::render('Contacts/Create', compact('organizations', 'countries'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreContactRequest $request)
+    public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'country_id' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'postal_code' => 'required',
+            'organization_id' => 'required',
+        ]);
+
+        $contact = Contact::create($data);
+
+        return redirect()->route('contacts.edit', $contact);
     }
 
     /**
--- resources/js/Pages/Contacts/Create.vue
+++ resources/js/Pages/Contacts/Create.vue
@@ -1,28 +1,169 @@
 <template>
-  <AppLayout title="Create Contact">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Create Contact
-            </h2>
-        </template>
+  <app-layout title="contacts">
+      <template #header>
+          <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+              Contacts
+          </h2>
+      </template>
 
-        <div>
-          <h1>Create Contact</h1>
-        </div>
-    </AppLayout>
+      <div class="container py-12">
+          <h1 class="text-3xl font-semibold text-gray-700 mb-3">
+              <span class="text-indigo-500">Contacts /</span> Create
+          </h1>
+
+
+          <div class="bg-white px-6 py-8 rounded-lg shadow">
+
+              <validation-errors class="mb-4" />
+
+              <div class="grid grid-cols-2 gap-6">
+
+                  <div>
+                      <label>
+                          First Name
+                          <Input v-model="contact.first_name" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Last Name
+                          <Input v-model="contact.last_name" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Organization
+                          <!-- <Input v-model="contact.organization_id" type="text" class="w-full" /> -->
+                          <select v-model="contact.organization_id" class="form-control w-full">
+
+                              <option value="" selected disabled>
+                                  Seleccione una opción
+                              </option>
+
+                              <option v-for="organization in organizations" :value="organization.id" :key="'organization-' + organization.id">
+                                  {{ organization.name }}
+                              </option>
+                          </select>
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Email
+                          <Input v-model="contact.email" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Phone
+                          <Input v-model="contact.phone" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Address
+                          <Input v-model="contact.address" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          City
+                          <Input v-model="contact.city" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          State
+                          <Input v-model="contact.state" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Country
+                          <!-- <Input v-model="contact.country_id" type="text" class="w-full" /> -->
+                          <select v-model="contact.country_id" class="form-control w-full">
+                              <option value="" selected disabled>
+                                  Seleccione una opción
+                              </option>
+                              <option v-for="country in countries" :value="country.id" :key="'country-' + country.id">
+                                  {{ country.name }}
+                              </option>
+                          </select>
+                      </label>
+                  </div>
+
+                  <div>
+                      <label>
+                          Postal Code
+                          <Input v-model="contact.postal_code" type="text" class="w-full" />
+                      </label>
+                  </div>
+
+              </div>
+
+              <div class="flex justify-end mt-3">
+                  <button class="btn btn-indigo" @click="store">
+                      Crear
+                  </button>
+              </div>
+
+          </div>
+      </div>
+  </app-layout>
 </template>
 
 <script>
-import AppLayout from '@/Layouts/AppLayout.vue';
+
+import AppLayout from '@/Layouts/AppLayout.vue'
+import Input from '@/Components/TextInput.vue'
+import ValidationErrors from '@/Components/ValidationErrors.vue'
 
 export default {
+  
   components: {
-    AppLayout
-  }
-}
+      AppLayout,
+      Input,
+      ValidationErrors
+  },
 
+  data() {
+      return {
+          contact: {
+              first_name: '',
+              last_name: '',
+              organization_id: '',
+              email: '',
+              phone: '',
+              address: '',
+              city: '',
+              state: '',
+              country_id: '',
+              postal_code: '',
+          }
+      }
+  },
+
+  props: {
+      organizations: Array,
+      countries: Array,
+  },
+
+  methods: {
+      store() {
+          this.$inertia.post(this.route('contacts.store'), this.contact);
+      }
+  }
+  
+}
 </script>
 
-<style scoped>
+<style>
 
 </style>
